de3fe5573cf02818be06c195f6aed5eead878317
Abraham Anyang <anyangmaker90@gmail.com>
2022-06-10 17:06:34 +0300

    commit tree 78d2534b94db9bebb318f1a04656ded72c516469

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = "bob"; //variale in global scope
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob';
+}
+
+
+const leastFavoriteCustomer="Anyang Maker";//global scope
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="Anyang Maker";
+}
